00e1e53bcb3a5b84ff33ac31dd93bd197f763a02
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2021-03-18 17:19:40 +0900

    little improvements

--- setup.sh
+++ setup.sh
@@ -13,18 +13,18 @@ fi
 if [ ! -z $3 ]; then
     server_serial=$3
 fi
-echo $servername
-echo $int_serial
-echo $server_serial
+echo "server name:$servername"
+echo "serial # for Int:$int_serial"
+echo "serial # for server:$server_serial"
 # ext/server.cnfのDNS.1と一致させる
 sed -e "s/dnsname/$servername/" ext/server.cnf.template > ext/server.cnf
 
 #各種鍵の保管場所作成
-dir=~/testca/
+dir=./workdir/
 cadir=${dir}ca/
 intdir=${dir}int/
 serverdir=${dir}server/
-mkdir ${dir} ${cadir} ${cadir}private ${intdir} ${intdir}private ${serverdir} ${serverdir}/private
+mkdir -p ${cadir} ${cadir}private ${intdir} ${intdir}private ${serverdir} ${serverdir}/private
 
 #プライベートルート証明書の作成
 if [ ! -f ${cadir}private/cakey.pem ]; then
@@ -32,7 +32,7 @@ openssl genrsa -out ${cadir}private/cakey.pem 2048
 openssl req  -new -x509 -key ${cadir}private/cakey.pem -sha256 -days 3650 -extensions v3_ca -out ${cadir}cacert.pem -subj "/C=JP/ST=Osaka/O=xxxcorp/CN=Example root CA"
 openssl x509 -inform PEM -outform DER -in ${cadir}cacert.pem -out ${cadir}cacert.der
 else 
-  echo "cakey.pem already exists. Skipping ca creation."
+  echo "cakey.pem already exists. Skipping root certificate creation."
 fi
 
 #中間証明書の作成
@@ -42,7 +42,7 @@ openssl req -new -key ${intdir}private/intkey.pem -sha256 -outform PEM -keyform 
 openssl x509 -extfile ext/intca.cnf -req -in ${intdir}int.csr -sha256 -CA ${cadir}cacert.pem -CAkey ${cadir}private/cakey.pem -set_serial $int_serial  -extensions v3_ca  -days 3650 -out ${intdir}intcert.pem
 openssl x509 -inform PEM -outform DER -in ${intdir}intcert.pem -out ${intdir}intcert.der
 else 
-  echo "intkey.pem already exists. Skipping intcert creation."
+  echo "intkey.pem already exists. Skipping intermediate certificate creation."
 fi
 
 #サーバ証明書の作成
@@ -51,6 +51,8 @@ openssl req -new -key ${serverdir}server.key -outform PEM -keyform PEM  -sha256 
 #openssl x509 -req -in ${serverdir}server.csr -sha256 -CA ${intdir}intcert.pem -CAkey ${intdir}private/intkey.pem -set_serial 01 -days 3650 -out ${serverdir}server.crt
 openssl x509 -extfile ext/server.cnf -req -in ${serverdir}server.csr -sha256 -CA ${intdir}intcert.pem -CAkey ${intdir}private/intkey.pem -set_serial $server_serial -days 3650 -extensions v3_server -out ${serverdir}server.crt
 
+# 出力先を空にする
+rm -fR ssl/*
 # cert chain 
 cat ${intdir}intcert.pem ${cadir}cacert.pem  > ssl/caint.crt
 cat ${serverdir}server.crt ${intdir}intcert.pem ${cadir}cacert.pem > ssl/all.crt
@@ -65,4 +67,4 @@ cp ${cadir}cacert.pem ssl/
 #AWS ACMへ証明書をインポートする
 #aws acm import-certificate --certificate file://${serverdir}server.crt --private-key file://${serverdir}server.key --certificate-chain file://${intdir}intcert.pem
 
-echo 'int_serial:'$int_serial' server_serial:'$server_serial > params/$servername.params
+echo 'int_serial:'$int_serial' server_serial:'$server_serial >> params/$servername.params
